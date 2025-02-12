9408c38b7473fd052b72ef26225de3a089fd9c2e
Sophie <sj483@sussex.ac.uk>
2025-02-12 11:42:01 +0000

    Modified WriteRegister.m ... ... to increase the number of subjects.

--- MakeTaskSets/WriteRegister.m
+++ MakeTaskSets/WriteRegister.m
@@ -1,7 +1,7 @@
 function [] = WriteRegister()
 
 rng(0);
-nSubjects = 50;
+nSubjects = 100;
 Register = GenRegister(nSubjects);
 for iSubject = 1:nSubjects
     webwrite('http://139.184.128.239/b01/WriteRegister.php',...
