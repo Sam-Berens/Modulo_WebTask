873aaee5626dda6be47c29e4bfd0e053af9dba7b
sam <scb40@sussex.ac.uk>
2025-05-30 18:05:54 +0100

    Updated GetTaskIO.php to require SubjectId input

--- GetTaskIO.php
+++ GetTaskIO.php
@@ -8,8 +8,17 @@ if ($Conn->connect_error) {
     die("Connection failed: " . $Conn->connect_error);
 }
 
+$Input = json_decode(file_get_contents('php://input'), true);
+if (!$Input) {
+	$Input = $_POST; // Only used when testing via MATLAB's webwrite function.
+}
+
+// Unpack the SubjectId:
+$SubjectId = $Input['SubjectId'];
+$SubjectId = mysqli_real_escape_string($Conn, $SubjectId);
+
 // Run the SELECT query
-$Sql = "SELECT * FROM TaskIO";
+$Sql = "SELECT * FROM TaskIO WHERE SubjectId='$SubjectId'";
 $Result = mysqli_query($Conn,$Sql);
 $TaskIO = mysqli_fetch_all($Result,MYSQLI_ASSOC);
 
